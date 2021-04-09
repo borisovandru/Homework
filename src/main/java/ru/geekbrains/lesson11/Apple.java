c4726848f44f261acdaef97aa1c205cf10727b77
borisovandru <80087707+borisovandru@users.noreply.github.com>
2021-04-09 22:42:43 +0400

    Initial commit

---
+++ src/main/java/ru/geekbrains/lesson11/Apple.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.lesson11;
+
+
+
+public class Apple extends Fruit {
+
+    Apple() {
+        super(1.0f);
+    }
+
+    @Override
+    public String toString(){
+        return "яблоки";
+    }
+}
---
+++ src/main/java/ru/geekbrains/lesson11/Fruit.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.lesson11;
+
+
+abstract class Fruit {
+    private final float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
